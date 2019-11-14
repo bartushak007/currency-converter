51ac2f9752610153d66adb8ce89350eb93cc67f4
Roman <rmnbrtshk@gmail.com>
2019-11-14 20:53:04 +0200

    Updates

---
+++ precache-manifest.6a89afb0a5e23edc29c408b3d4206c42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23313c93b120ea4da5dfa5631ce96cc6",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "f7f43969fd59eafff595",
+    "url": "/currency-converter/static/css/main.99a73826.chunk.css"
+  },
+  {
+    "revision": "413481fa5a745247fb96",
+    "url": "/currency-converter/static/js/2.079503fe.chunk.js"
+  },
+  {
+    "revision": "f7f43969fd59eafff595",
+    "url": "/currency-converter/static/js/main.f6f788e3.chunk.js"
+  },
+  {
+    "revision": "5e49065664d1b26c094d",
+    "url": "/currency-converter/static/js/runtime-main.49b368cc.js"
+  }
+]);
